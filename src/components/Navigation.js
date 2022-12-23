507d743c8ce3ad3ef334a00a60f8cb4810cc7cae
sanchitajagtap <jsanchita26@gmial.com>
2022-12-23 23:21:54 +0530

    reactjs live

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return(
+        <div className="row navbar">
+            <div className="col-sm-12 text-sm-end">
+                <NavLink className="navlinks" activeClassName exact = "true" to="/"> Home </NavLink>
+                
+                <NavLink className="navlinks" activeClassName to="/event-handlers"> contact </NavLink>
+              
+                <NavLink className="navlinks" activeClassName to="/form-validation"> About us </NavLink>
+              
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
